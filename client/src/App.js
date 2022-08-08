98ca2cca6162b642ba5cc4282250eed34fe95767
Jun <jun279r@gmail.com>
2022-08-08 22:21:38 +0200

    Add button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import ChooseGameMode from "./components/ChooseGameMode";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1 className="text-3xl font-bold text-pale-custom">Hello world!</h1>
+      <ChooseGameMode />
     </div>
   );
 }
---
+++ client/src/components/ChooseGameMode.js
@@ -0,0 +1,11 @@
+function ChooseGameMode() {
+  return (
+    <div className="bg-grey-custom grid h-screen place-items-center max-w-4xl mx-auto max-h-full">
+      <button type="button" className="rounded-full bg-slate-300">
+        Start game
+      </button>
+    </div>
+  );
+}
+
+export default ChooseGameMode;
