f3f9c946ecc22646c16f9384f600633ab897b187
Jun <jun279r@gmail.com>
2022-08-15 19:16:00 +0200

    Add ConsoleLog component

---
+++ client/src/components/ConsoleLog.js
@@ -0,0 +1,9 @@
+function ConsoleLog(params) {
+  return (
+    <div className="bg-wine-custom text-white">
+      <p className="p-8">What should pokemon do?</p>
+    </div>
+  );
+}
+
+export default ConsoleLog;
--- client/src/components/battle-components/ConsoleMenu.js
+++ client/src/components/battle-components/ConsoleMenu.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { PokemonContext } from "../Battleground";
 import BattleMenu from "./BattleMenu";
+import ConsoleLog from "../ConsoleLog";
 
 function ConsoleMenu({}) {
   // myMoves, enemyMoves
@@ -11,7 +12,9 @@ function ConsoleMenu({}) {
 
   return (
     <div className="grid grid-cols-6 border-8 border-pale-custom">
-      <div className="col-start-1 col-end-5 border-r-4">Console</div>
+      <div className="col-start-1 col-end-5 border-r-8 border-pale-custom grid place-items-stretch">
+        <ConsoleLog />
+      </div>
       <div className="col-start-5 col-end-7 grid place-items-stretch">
         <BattleMenu />
       </div>
